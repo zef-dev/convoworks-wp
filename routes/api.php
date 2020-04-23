eb6eb611753b481ed0bd44cc15e7a4ea62786de5
zbiskup <zbiskup@gmail.com>
2020-04-23 11:16:04 +0200

    Open route for now, WIP

--- routes/api.php
+++ routes/api.php
@@ -3,9 +3,9 @@
 // Define the namespace for the API controllers
 $namespace = 'ConvoPlugin\Http\Api';
 
-register_rest_route('convo/v1',
-	'/services',
-	['methods' => 'GET',    'callback' => [$namespace . '\ServicesController', 'index'],
-	 'permission_callback' => function () {
-		 return current_user_can('manage_options');
-	 }]);
+
+// services route
+register_rest_route('convo/v1', '/services', [
+	'methods' => 'GET',
+	'callback' => [$namespace . '\ServicesController', 'index']
+]);
