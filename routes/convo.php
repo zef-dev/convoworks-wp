c840f640974703f9decb5211d78761d0ce8bf0e0
zbiskup <zbiskup@gmail.com>
2020-04-23 10:51:29 +0200

    Removed obsolete file

--- routes/convo.php
+++
@@ -1,9 +0,0 @@
-<?php
-/**
- * Here we are registering all routes need for Convo library admin
- *
- * We are using  inpsyde/WP-REST-Starter
- *
- * More info on https://github.com/inpsyde/WP-REST-Starter
- */
-
