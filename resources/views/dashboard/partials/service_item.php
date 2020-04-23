ec011d0a071c2560cd89749d58e719256f5d54b9
zbiskup <zbiskup@gmail.com>
2020-04-23 10:40:35 +0200

    Service item link, styling

--- resources/views/dashboard/partials/service_item.php
+++ resources/views/dashboard/partials/service_item.php
@@ -2,7 +2,12 @@
 	<div>
 		<div class="opd-product-name d-flex align-items-center">
 			<div>
-				<h4><?php /** @var $service */ echo $service->name ?></h4>
+				<h4>
+                    <?php
+                        /** @var $service */
+                        echo '<a href="' . admin_url() . '">' . $service->name . '</a>';
+                    ?>
+                </h4>
 			</div>
 		</div>
 	</div>
