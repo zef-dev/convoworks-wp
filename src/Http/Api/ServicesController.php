03eb9f159eb80d4fba6e7a5c5fa360f7086d3246
Zvonko Biškup <zbiskup@gmail.com>
2020-08-27 11:00:34 +0200

    Service cleanup

--- src/Http/Api/ServicesController.php
+++ src/Http/Api/ServicesController.php
@@ -5,19 +5,15 @@ namespace ConvoPlugin\Http\Api;
 use Convo\Core\Admin\AdminRestApi;
 use Convo\Core\Admin\AdminUser;
 use Convo\Core\IAdminUser;
-use Convo\Core\Rest\NotFoundException;
 use GuzzleHttp\Psr7\Uri;
 use Inpsyde\WPRESTStarter\Core\Request\Request;
 use WP_REST_Request;
-use function ConvoPlugin\startsWith;
 
 class ServicesController extends Controller
 {
 	public static function all(WP_REST_Request $request)
 	{
-		$uri = '';
 		$route = $request->get_route();
-		$justRoute = substr($route, 10, strlen($route));
 
 		$uri = new Uri( CONVOWP_URL . '/wp-json' . $route);
 
