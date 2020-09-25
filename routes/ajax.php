201bc38ebaaa3064e33f409fd0b2e6bb17cd5b7a
Zvonko Biškup <zbiskup@gmail.com>
2020-09-25 10:12:41 +0200

    Fixed full screen

--- routes/ajax.php
+++ routes/ajax.php
@@ -5,4 +5,6 @@ $settingsController = new \ConvoPlugin\Http\SettingsController;
 // Settings
 add_action('wp_ajax_opd_update_settings', function() use ($settingsController) { $settingsController->update(); });
 
+add_action('wp_ajax_op3toggleFullScreen', function() use ($settingsController) { $settingsController->toggleFullScreen(); });
+
 
