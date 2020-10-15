235968299f526aeba85714e96e1a68341f456d70
Zvonko Biškup <zbiskup@gmail.com>
2020-10-15 10:53:01 +0200

    Changed version fo the plugin for the demo

--- convo-plugin.php
+++ convo-plugin.php
@@ -19,7 +19,7 @@ use ConvoPlugin\Providers\ConvoWPPlugin;
 // Add autoloader
 require_once __DIR__.'/vendor/autoload.php';
 
-define('CONVOWP_VERSION', '0.0.2');
+define('CONVOWP_VERSION', '0.0.3');
 define('CONVOWP_PLUGIN_SLUG', plugin_basename(__FILE__));
 define('CONVOWP_FILE', __FILE__);
 define('CONVOWP_PATH', __DIR__);
