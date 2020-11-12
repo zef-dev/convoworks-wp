76cde04496760698fc6d3f2db4137df8445b8a0e
Zvonko Biškup <zbiskup@gmail.com>
2020-11-13 00:40:21 +0100

    Adding JavaScript and CSS only on Vonvo WP admin pages

--- src/Providers/AssetsProvider.php
+++ src/Providers/AssetsProvider.php
@@ -25,35 +25,51 @@ class AssetsProvider
         add_action('admin_enqueue_scripts', [$this, 'enqueueAdminAssets']);
     }
 
-    /**
-     * Enqueue admin scripts
-     */
-    public function enqueueAdminAssets()
+	/**
+	 * Enqueue admin scripts
+	 *
+	 * @param $page
+	 */
+    public function enqueueAdminAssets($page)
     {
-    	global $wp_version;
+	    $pagesNeedingAppJs = [
+		    'toplevel_page_convo-plugin',
+		    'convo-plugin#!/convoworks-editor',
+		    'convowp_page_convo-settings',
+	    ];
 
-	    // The "updates" dependency breaks some stuff on older WP versions
-	    if (version_compare($wp_version, '5.5', '>=')) {
-		    wp_enqueue_script('convo-plugin-dashboard',  plugins_url('public/assets/js/app.js', CONVOWP_FILE), ['jquery'], $this->version());
-	    } else {
-		    wp_enqueue_script('convo-plugin-dashboard',  plugins_url('public/assets/js/app.js', CONVOWP_FILE), ['jquery', 'updates'], $this->version());
+	    // Although we could check for suitable pages by checking if
+	    // "op-funnels" string exists, by checking against the array decision
+	    // on which page to show JS needs to be conscious and not by default
+	    if (in_array($page, $pagesNeedingAppJs)) {
+		    global $wp_version;
+
+		    // The "updates" dependency breaks some stuff on older WP versions
+		    if ( version_compare( $wp_version, '5.5', '>=' ) ) {
+			    wp_enqueue_script( 'convo-plugin-dashboard', plugins_url( 'public/assets/js/app.js', CONVOWP_FILE ), [ 'jquery' ], $this->version() );
+		    } else {
+			    wp_enqueue_script( 'convo-plugin-dashboard', plugins_url( 'public/assets/js/app.js', CONVOWP_FILE ), [
+				    'jquery',
+				    'updates'
+			    ], $this->version() );
+		    }
+
+		    // Add some required variables to our global script
+		    wp_localize_script( "convo-plugin-dashboard", 'ConvoScriptData', [
+			    'ajax_url' => admin_url( 'admin-ajax.php' ),
+			    'nonce'    => wp_create_nonce( 'wp_rest' ),
+		    ] );
+
+		    wp_enqueue_style( "convo-framework", plugins_url( "public/assets/css/framework.css", CONVOWP_FILE ), [], $this->version() );
+		    wp_enqueue_style( "convo-app", plugins_url( "public/assets/css/app.css", CONVOWP_FILE ), [], $this->version() );
+		    wp_enqueue_style( "convo-plugin-dashboard", plugins_url( "public/assets/css/convo-all.css", CONVOWP_FILE ), [], $this->version() );
+
+		    remove_all_actions( "admin_notices" );
+
+		    if ( is_admin() ) {
+			    wp_enqueue_style( "convo-wp-dashboard", plugins_url( "public/assets/css/wp.css", CONVOWP_FILE ), [], $this->version() );
+		    }
 	    }
-
-	    // Add some required variables to our global script
-        wp_localize_script("convo-plugin-dashboard", 'ConvoScriptData', [
-            'ajax_url' => admin_url('admin-ajax.php'),
-            'nonce'    => wp_create_nonce('wp_rest'),
-        ]);
-
-        wp_enqueue_style("convo-framework",        plugins_url("public/assets/css/framework.css", CONVOWP_FILE), [], $this->version());
-        wp_enqueue_style("convo-app",        plugins_url("public/assets/css/app.css", CONVOWP_FILE), [], $this->version());
-        wp_enqueue_style("convo-plugin-dashboard", plugins_url("public/assets/css/convo-all.css",       CONVOWP_FILE), [], $this->version());
-
-        remove_all_actions("admin_notices");
-
-        if (is_admin()) {
-            wp_enqueue_style("convo-wp-dashboard", plugins_url("public/assets/css/wp.css", CONVOWP_FILE), [], $this->version());
-        }
     }
 
     /**
