ba1a3cfc1a5c95e039ae844a911aafdb6cef9675
Zvonko Biškup <zbiskup@gmail.com>
2020-11-24 10:48:19 +0100

    Removed top header

--- resources/views/legacy/index.php
+++ resources/views/legacy/index.php
@@ -5,7 +5,7 @@ if ( ! defined('ABSPATH')) {
 ?>
 
 <div class="opd-dashboard convo-proto-content">
-	<?php \ConvoPlugin\partial('partials/navigation'); ?>
+	<?php //\ConvoPlugin\partial('partials/navigation'); ?>
 
     <div class="opd-dashboard-connected layout convoworks">
 
