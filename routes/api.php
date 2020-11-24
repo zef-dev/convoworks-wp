269e50aee2019d097ec418f20eb56a41fd5d8a23
Zvonko Biškup <zbiskup@gmail.com>
2020-11-24 11:34:40 +0100

    Added catch all REST routes

--- routes/api.php
+++ routes/api.php
@@ -3,151 +3,17 @@
 // Define the namespace for the API controllers
 $namespace = 'ConvoPlugin\Http\Api';
 
-register_rest_route('convo/v1', '/services', [
-	'methods' => 'GET',
+// admin routes catch all
+register_rest_route('convo/v1', '/(?P<serviceId>[\S]+)', [
+	'methods' => ['GET', 'POST', 'PUT', 'DELETE'],
 	'callback' => [$namespace . '\ServicesController', 'all'],
 	'permission_callback' => function ($request) {
 		return current_user_can('publish_posts');
 	},
 ]);
 
-register_rest_route('convo/v1', '/services/(?P<serviceId>[\w-]+)', [
-	'methods' => 'GET',
-	'callback' => [$namespace . '\ServicesController', 'all'],
-	'permission_callback' => function ($request) {
-		return current_user_can('publish_posts');
-	},
-]);
-
-register_rest_route('convo/v1', '/service-platform-propagate/(?P<serviceId>[\w-]+)/(?P<serviceType>[\w-]+)', [
-	'methods' => 'GET',
-	'callback' => [$namespace . '\ServicesController', 'all'],
-	'permission_callback' => function ($request) {
-		return current_user_can('publish_posts');
-	},
-]);
-
-register_rest_route('convo/v1', '/service-platform-config/(?P<serviceId>[\w-]+)/(?P<serviceType>[\w-]+)', [
-	'methods' => 'GET',
-	'callback' => [$namespace . '\ServicesController', 'all'],
-	'permission_callback' => function ($request) {
-		return current_user_can('publish_posts');
-	},
-]);
-
-register_rest_route('convo/v1', '/service-platform-config/(?P<serviceId>[\w-]+)', [
-	'methods' => 'GET',
-	'callback' => [$namespace . '\ServicesController', 'all'],
-	'permission_callback' => function ($request) {
-		return current_user_can('publish_posts');
-	},
-]);
-
-register_rest_route('convo/v1', '/services/(?P<serviceId>[\w-]+)/meta', [
-	'methods' => 'GET',
-	'callback' => [$namespace . '\ServicesController', 'all'],
-	'permission_callback' => function ($request) {
-		return current_user_can('publish_posts');
-	},
-]);
-
-register_rest_route('convo/v1', '/services/(?P<serviceId>[\w-]+)/preview', [
-	'methods' => 'GET',
-	'callback' => [$namespace . '\ServicesController', 'all'],
-	'permission_callback' => function ($request) {
-		return current_user_can('publish_posts');
-	},
-]);
-
-register_rest_route('convo/v1', '/service-test/(?P<serviceId>[\w-]+)', [
-	'methods' => 'POST',
-	'callback' => [$namespace . '\ServicesController', 'all'],
-	'permission_callback' => function ($request) {
-		return current_user_can('publish_posts');
-	},
-]);
-
-register_rest_route('convo/v1', '/user-packages', [
-	'methods' => 'GET',
-	'callback' => [$namespace . '\ServicesController', 'all'],
-	'permission_callback' => function ($request) {
-		return current_user_can('publish_posts');
-	},
-]);
-
-register_rest_route('convo/v1', '/service-releases/(?P<serviceId>[\w-]+)', [
-	'methods' => 'GET',
-	'callback' => [$namespace . '\ServicesController', 'all'],
-	'permission_callback' => function ($request) {
-		return current_user_can('publish_posts');
-	},
-]);
-
-register_rest_route('convo/v1', '/service-versions/(?P<serviceId>[\w-]+)', [
-	'methods' => 'GET',
-	'callback' => [$namespace . '\ServicesController', 'all'],
-	'permission_callback' => function ($request) {
-		return current_user_can('publish_posts');
-	},
-]);
-
-register_rest_route('convo/v1', '/service-packages/(?P<serviceId>[\w-]+)', [
-	'methods' => ['GET', 'POST', 'DELETE'],
-	'callback' => [$namespace . '\ServicesController', 'all'],
-	'permission_callback' => function ($request) {
-		return current_user_can('publish_posts');
-	},
-]);
-
-register_rest_route('convo/v1', '/services/(?P<serviceId>[\w-]+)', [
-	'methods' => ['PUT', 'DELETE'],
-	'callback' => [$namespace . '\ServicesController', 'all'],
-	'permission_callback' => function ($request) {
-		return current_user_can('publish_posts');
-	},
-]);
-
-register_rest_route('convo/v1', '/services', [
-	'methods' => 'POST',
-	'callback' => [$namespace . '\ServicesController', 'all'],
-	'permission_callback' => function ($request) {
-		return current_user_can('publish_posts');
-	},
-]);
-
-register_rest_route('convo/v1', '/templates', [
-	'methods' => 'GET',
-	'callback' => [$namespace . '\ServicesController', 'all'],
-	'permission_callback' => function ($request) {
-		return current_user_can('publish_posts');
-	},
-]);
-
-register_rest_route('convo/v1', '/config-options', [
-	'methods' => 'GET',
-	'callback' => [$namespace . '\ServicesController', 'all'],
-	'permission_callback' => function ($request) {
-		return current_user_can('publish_posts');
-	},
-]);
-
-register_rest_route('convo/v1', '/service-platform-config/(?P<serviceId>[\w-]+)/(?P<platform>[\w-]+)', [
-	'methods' => ['PUT', 'POST'],
-	'callback' => [$namespace . '\ServicesController', 'all'],
-	'permission_callback' => function ($request) {
-		return current_user_can('publish_posts');
-	},
-]);
-
-register_rest_route('convo/v1', '/service-platform-propagate/(?P<serviceId>[\w-]+)/(?P<platform>[\w-]+)', [
-	'methods' => 'POST',
-	'callback' => [$namespace . '\ServicesController', 'all'],
-	'permission_callback' => function ($request) {
-		return current_user_can('publish_posts');
-	},
-]);
-
-register_rest_route('convo/v1/public', '/service-run/(?P<platform>[\w-]+)/a/(?P<serviceId>[\w-]+)', [
+// public routes catch all
+register_rest_route('convo/v1/public', '/(?P<serviceId>[\S]+)', [
 	'methods' => 'POST',
 	'callback' => [$namespace . '\ServicesController', 'publicRoutes'],
 	'permission_callback' => function ($request) {
