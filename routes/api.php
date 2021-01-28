64f4023829107d2c330e2cdd9365e7e3900396d2
Zvonko Biškup <zbiskup@gmail.com>
2021-01-28 11:36:50 +0100

    Hack for service export/import route

--- routes/api.php
+++ routes/api.php
@@ -12,6 +12,31 @@ register_rest_route('convo/v1', '/public/(?P<serviceId>[\S]+)', [
 	},
 ]);
 
+// SPECIAL ROUTES HANDLED differently
+register_rest_route('convo/v1', '/service-imp-exp/export/(?P<serviceId>[\S]+)', [
+	'methods' => ['GET', 'POST', 'PUT', 'DELETE'],
+	'callback' => [$namespace . '\ServicesController', 'specialRoutes'],
+	'permission_callback' => function ($request) {
+		return true;
+	},
+]);
+
+register_rest_route('convo/v1', '/service-imp-exp/import/(?P<serviceId>[\S]+)', [
+	'methods' => ['GET', 'POST', 'PUT', 'DELETE'],
+	'callback' => [$namespace . '\ServicesController', 'specialRoutes'],
+	'permission_callback' => function ($request) {
+		return true;
+	},
+]);
+
+register_rest_route('convo/v1', '/service-imp-exp/export/(?P<serviceId>[\S]+)/(?P<platformId>[\S]+)', [
+	'methods' => ['GET', 'POST', 'PUT', 'DELETE'],
+	'callback' => [$namespace . '\ServicesController', 'specialRoutes'],
+	'permission_callback' => function ($request) {
+		return true;
+	},
+]);
+
 // admin routes catch all
 register_rest_route('convo/v1', '/(?P<serviceId>[\S]+)', [
 	'methods' => ['GET', 'POST', 'PUT', 'DELETE'],
