e42a10c60a52da230370a4e4b0038d7b327dc045
Zvonko Biškup <zbiskup@gmail.com>
2021-02-04 09:39:12 +0100

    Beta build, Amazon callback URL

--- resources/views/settings/partials/amazon.php
+++ resources/views/settings/partials/amazon.php
@@ -40,7 +40,7 @@ $amazonVendorId     = isset($userSettings['amazon']['vendor_id']) ? $userSetting
 
         <div class="ops-form-group">
             <label for="opd_facebook_app_secret">Amazon Oauth Callback URL</label>
-            <input type="text" value="<?php echo ConvoPlugin\oauth_callback_url() ?>" class="ops-form-control" disabled>
+            <input type="text" value="<?php echo \get_rest_url(null, 'convo/v1/admin-auth/amazon') ?>" class="ops-form-control" disabled>
         </div>
 
 		<div class="ops-form-actions">
