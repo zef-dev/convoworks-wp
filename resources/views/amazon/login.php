95932716af013fff8449bb645496a9acb839a27c
Zvonko Biškup <zbiskup@gmail.com>
2021-03-02 09:40:47 +0100

    Login template

--- resources/views/amazon/login.php
+++ resources/views/amazon/login.php
@@ -13,5 +13,14 @@
     // change the header to 200 OK
     header("HTTP/1.1 200 OK");
 
-    include(CONVOWP_PATH . '/resources/views/amazon/partials/loginForm.php');
-    exit;
+    $user = wp_get_current_user();
+
+    $user = new \ConvoPlugin\Convo\Wp\AdminUser($user);
+
+    if (! empty($user)) {
+	    include(CONVOWP_PATH . '/resources/views/amazon/partials/loggedIn.php');
+	    exit;
+    } else {
+	    include(CONVOWP_PATH . '/resources/views/amazon/partials/loginForm.php');
+	    exit;
+    }
