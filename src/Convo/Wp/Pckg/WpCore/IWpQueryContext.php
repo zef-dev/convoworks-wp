20a235d0c4c38abd2185f1b6528cf013195b7a13
Tihomir Dmitrovic <tole.car@gmail.com>
2021-03-02 23:07:29 +0100

    added docs

--- src/Convo/Wp/Pckg/WpCore/IWpQueryContext.php
+++ src/Convo/Wp/Pckg/WpCore/IWpQueryContext.php
@@ -3,6 +3,13 @@
 namespace Convo\Wp\Pckg\WpCore;
 
 
+/**
+ * @author Tole
+ *
+ * WpQuery context serves for navigating and looping through WP_Query in a conversational manner. That means that the 
+ * paging and selection information is preserved through session.
+ * The selection has to be implemented in such way that the wp post functions can work as in the loop. 
+ */
 interface IWpQueryContext 
 {
     
@@ -14,31 +21,71 @@ interface IWpQueryContext
     public function getWpQuery();
     
     /**
+     * Returns iterator over the current wp query.
      * @return \Generator
      */
     public function getLoopIterator();
     
     // ACTIONS - PAGES
+    /**
+     * Moves page pointer to the next page. If not available, will throw exception.
+     * @throws NavigateOutOfRangeException
+     */
     public function moveNextPage();
     
+    /**
+     * Moves page pointer to the previous page. If not available, will throw exception.
+     * @throws NavigateOutOfRangeException
+     */
     public function movePreviousPage();
     
+    /**
+     * Resets both the pagination and selection to initial state.
+     */
     public function resetNavi();
     
     // ACTIONS - POSTS SELECTION
+    /**
+     * Selects current page post. If post at the given index does not exists, will throw exception.
+     * @param int $index
+     * @throws NavigateOutOfRangeException
+     */
     public function selectPagePost( $index);
     
+    /**
+     * Selects last post on the current page.
+     * @param int $index
+     */
     public function selectLastPagePost();
     
+    /**
+     * Selects previous post if available. If the previous post is on previous page, it will move the page pointer too.
+     * @throws NavigateOutOfRangeException
+     */
     public function selectPreviousPost();
     
+    /**
+     * Selects next post if available. If the next post is on the next page, it will move the page pointer too.
+     * @throws NavigateOutOfRangeException
+     */
     public function selectNextPost();
     
+    /**
+     * Selects last selected post again - making it the current in the loop.
+     */
     public function restoreSelectedPost();
     
     // INFO
+    /**
+     * Returns loop page info array.
+     * @return array
+     */
     public function getLoopPageInfo();
     
+    /**
+     * Returns loop post info array.
+     * @return array
+     */
     public function getLoopPostInfo();
     
 
