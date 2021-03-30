bb17529acd6f5965fb4c101129e6bf6411075e0e
Tihomir Dmitrovic <tole.car@gmail.com>
2021-03-30 09:45:12 +0200

    version fix - no suffix

--- convo-plugin.php
+++ convo-plugin.php
@@ -8,7 +8,7 @@
  * UID: convo-wp
  * Plugin URI: https://convoworks.com
  * Author: ZEF Development
- * Version: 0.21.0-beta4
+ * Version: 0.21.0
  * Author URI: https://zef.dev
  * Text Domain: convo-wp
  * Domain Path: /resources/lang
@@ -16,7 +16,7 @@
 
 use ConvoPlugin\Providers\ConvoWPPlugin;
 
-define('CONVOWP_VERSION', '0.21.0-beta3');
+define('CONVOWP_VERSION', '0.21.0');
 define('CONVOWP_PLUGIN_SLUG', plugin_basename(__FILE__));
 define('CONVOWP_FILE', __FILE__);
 define('CONVOWP_PATH', __DIR__);
