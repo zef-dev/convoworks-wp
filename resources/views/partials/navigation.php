429347af502954c332953c6fc17c5dec0f009650
Zvonko Biškup <zbiskup@gmail.com>
2021-04-27 11:14:23 +0200

    Settings screen header adjustments

--- resources/views/partials/navigation.php
+++ resources/views/partials/navigation.php
@@ -4,29 +4,14 @@ if ( ! defined('ABSPATH')) {
 }
 ?>
 
-<div class="ops-top-menu d-flex justify-content-between align-items-center">
+
+<div class="ops-top-menu d-flex justify-content-between align-items-center" style="height: 90px; background-color: #2F74EA;">
     <nav class="ops-left pl-4">
         <ul>
             <li class="d-flex align-items-center">
-                <a class="ops-logo" href="<?php echo admin_url( 'admin.php?page=convo-plugin' ); ?>">
-                    <img style="margin-top: 5px;" src="<?php echo CONVOWP_ASSETS_URL?>/images/convowp-logo.png" alt=""Convoworks WP Logo>
-                </a>
-            </li>
-        </ul>
-    </nav>
-
-    <nav class="ops-right">
-        <nav class="ops-main-menu ops_movingBorderMenu">
-            <ul class="ops-right-menu">
-                <?php \Convo\Services\Menus::find('convo-plugin')->render(); ?>
-            </ul>
-            <span class="ops_movingBorderMenuHover ops-main-menu-hover"></span>
-        </nav>
-
-        <ul class="ops-right-static">
-            <li class="ops-fullscreenBtn">
-                <a href="#">
-                    <i class="ops-iconFont ops-fullscreen-fill-icon"></i>
+                <a class="ops-logo" style="margin-top: 15px; margin-left: 15px;" href="<?php echo admin_url( 'admin.php?page=convo-plugin' ); ?>">
+                    <img width="35" height="22" src="<?php echo CONVOWP_ASSETS_URL?>/images/866fea890596f67cd1770b2686351816.png?866fea890596f67cd1770b2686351816)" alt=""Convoworks Logo>
+                    <div style="margin-left: 7px;font-family: DM Sans, sans-serif;width: 11px; color:#fff;display: inline;font-size: 15px;font-weight: bold;">Convoworks</div>
                 </a>
             </li>
         </ul>
