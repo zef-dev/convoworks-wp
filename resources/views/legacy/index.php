5cd0dc207af84420aa3bf924fc6ccf18a108ae04
Zvonko Biškup <zbiskup@gmail.com>
2021-05-10 10:48:12 +0200

    Updated all to the latest dev versions

--- resources/views/legacy/index.php
+++ resources/views/legacy/index.php
@@ -11,9 +11,9 @@ if ( ! defined('ABSPATH')) {
 
 
 
-        <script type="text/javascript" src="<?php echo CONVOWP_ASSETS_URL ?>js/vendor.js?813ff73fe9084fa2125b"></script>
+        <script type="text/javascript" src="<?php echo CONVOWP_ASSETS_URL ?>js/vendor.js?6fe342564f2f62eaa4b8"></script>
 
-        <script type="text/javascript" src="<?php echo CONVOWP_ASSETS_URL ?>js/main.js?813ff73fe9084fa2125b"></script>
+        <script type="text/javascript" src="<?php echo CONVOWP_ASSETS_URL ?>js/main.js?6fe342564f2f62eaa4b8"></script>
 
         <script type="text/javascript">
                 <?php
