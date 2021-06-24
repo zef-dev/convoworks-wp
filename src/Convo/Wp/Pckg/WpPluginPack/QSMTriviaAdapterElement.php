8542c21ea46613793e649e49ad49a5710dd8775a
Marko Klobučar Ledinšćak <mrqoqlobo@gmail.com>
2021-06-24 16:04:05 +0200

    QSM Adapter placeholder

---
+++ src/Convo/Wp/Pckg/WpPluginPack/QSMTriviaAdapterElement.php
@@ -0,0 +1,8 @@
+<?php declare(strict_types=1);
+
+namespace Convo\Wp\Pckg\WpPluginPack;
+
+class QSMTriviaAdapterElement extends \Convo\Core\Workflow\AbstractWorkflowContainerComponent implements \Convo\Core\Workflow\IConversationElement
+{
+    
+}
