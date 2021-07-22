bdaf362c2a0c089e2ea4e5feda3882cd86dbbf67
mmarcec007 <mark.marcec@gmail.com>
2021-07-22 09:30:09 +0200

    Add getting started WP Page

--- routes/admin.php
+++ routes/admin.php
@@ -12,4 +12,5 @@ Route::adminPage(__('Convoworks WP', 'convo-wp'),       'convo-plugin',         
 //Route::adminSubPage(__('Services',  'convo-wp' ), 'convo-plugin', 'convo-plugin', 'DashboardController@index', ['position' => 10]);
 //Route::adminSubPage(__('Services',  'convo-wp' ), 'convo-plugin', 'convo-legacy', 'LegacyController@index', ['position' => 10]);
 Route::adminSubPage(__('Settings',  'convo-wp' ), 'convo-plugin', 'convo-settings', 'SettingsController@index', ['position' => 20]);
+Route::adminSubPage(__('Getting Started',  'convo-wp' ), 'convo-plugin', 'convo-getting-started', 'GettingStartedController@index', ['position' => 20]);
 Route::adminSubPage(__('Service Single',  'convo-wp' ), '', 'convo-service-single', 'ServicesController@single', ['position' => 20]);
