6d0e2af5e6f8260ab5ccb59711e8bd21a83a1082
mmarcec007 <mark.marcec@gmail.com>
2021-07-27 15:03:18 +0200

    Change id of WP_Query album mp3 source

--- src/Convo/Wp/Pckg/WpPluginPack/WpPluginPackPackageDefinition.php
+++ src/Convo/Wp/Pckg/WpPluginPack/WpPluginPackPackageDefinition.php
@@ -80,7 +80,7 @@ class WpPluginPackPackageDefinition extends AbstractPackageDefinition
 					'id' => array(
 						'editor_type' => 'text',
 						'editor_properties' => array(),
-						'defaultValue' => 'search_album_media',
+						'defaultValue' => 'search_media',
 						'name' => 'Context ID',
 						'description' => 'Unique ID by which this context is referenced',
 						'valueType' => 'string'
