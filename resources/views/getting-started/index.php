622b2c620c896a749b8fd2c9d056f5795fa14b84
Tihomir Dmitrovic <tole.car@gmail.com>
2021-08-04 09:13:12 +0200

    Fix left margin for getting started view

--- resources/views/getting-started/index.php
+++ resources/views/getting-started/index.php
@@ -6,10 +6,10 @@ if ( ! defined('ABSPATH')) {
 $is_connected_to_amazon = $is_connected_to_amazon ?? false;
 ?>
 
-<div class="opd-dashboard">
+<div class="opd-dashboard" style="margin-left: -20px;">
 	<?php Convo\partial('partials/navigation'); ?>
 
-    <div class="opd-dashboard-getting-started p-4">
+    <div class="opd-dashboard-settings p-4">
         <div class="text-center">
             <h1>Welcome to Convoworks WP</h1>
             <p>Thank you for choosing Convoworks WP - the latest drag & drop WordPress conversational service builder in the market.</p>
