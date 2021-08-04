e98d5c10cfcd63248a8ba4d2831161c003e0591c
Tihomir Dmitrovic <tole.car@gmail.com>
2021-08-04 10:46:47 +0200

    Text fix

--- resources/views/getting-started/index.php
+++ resources/views/getting-started/index.php
@@ -30,7 +30,7 @@ $is_connected_to_amazon = $is_connected_to_amazon ?? false;
                 <div class="card">
                     <div class="card-body">
                         <h5 class="card-title">Create Your First Service</h5>
-                        <p class="card-text">Convoworks makes it easy to create conversational services in WordPress. You can read our guide on how create your first form or start creating your first service on your own.</p>
+                        <p class="card-text">Convoworks makes it easy to create conversational services in WordPress. You can read our guide on how to create your first form or start creating your first service on your own.</p>
                         <a href="<?php echo admin_url('admin.php?page=convo-plugin#!/add-new-service') ?>" class="btn btn-primary">Create your first service</a>
                         <a href="https://convoworks.com/docs/publishers/tutorial-getting-started/" target="_blank" class="btn btn-outline-secondary">Read the full guide</a>
                     </div>
