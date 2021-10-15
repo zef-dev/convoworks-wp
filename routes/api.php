8c02ce6c562ae9f2887aefad72f8dbacc897fc4e
Marko Klobučar Ledinšćak <mrqoqlobo@gmail.com>
2021-10-15 16:46:36 +0200

    Reinstante media handle for media download

--- routes/api.php
+++ routes/api.php
@@ -31,6 +31,28 @@ register_rest_route('convo/v1', '/public/(?P<serviceId>[\S]+)', [
 	},
 ]);
 
+// media
+register_rest_route('convo/v1', '/media/(?P<serviceId>[\S]+)/(?P<mediaId>[\S]+)/download', [
+	'methods' => ['GET'],
+// 	'callback' => [$namespace . '\ServicesController', 'all'],
+    'callback' => function ( $data) {
+        
+        $base_path  =   '/convoworks/services/'.$data['serviceId'].'/media/'.$data['mediaId'];
+        $json       =   json_decode( file_get_contents( wp_upload_dir()['basedir'].$base_path.'.json'), true);
+        $ext        =   $json['ext'];
+        
+        $response = new WP_REST_Response();
+        $response->set_status( 301);
+        
+        $response->header( 'Location', wp_upload_dir()['baseurl'].$base_path.'.'.$ext);
+        return $response;
+    },
+	'permission_callback' => function ($request) {
+		return true;
+	},
+]);
+// end media
+
 // Handle service-media differently
 register_rest_route('convo/v1', '/public/service-media/(?P<serviceId>[\S]+)/(?P<mediaId>[\S]+)', [
 	'methods' => ['GET'],
