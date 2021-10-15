f1b6ce18fb55157a641d20bbaa67ac9ee6a03b8c
Marko Klobučar Ledinšćak <mrqoqlobo@gmail.com>
2021-10-15 16:05:52 +0200

    Handle `service-media`

--- routes/api.php
+++ routes/api.php
@@ -22,32 +22,19 @@ register_rest_route('convo/v1', '/oauth/(?P<type>[\S]+)/(?P<serviceId>[\S]+)', [
 ]);
 // END Oauth
 
-// media
-register_rest_route('convo/v1', '/media/(?P<serviceId>[\S]+)/(?P<mediaId>[\S]+)/download', [
-	'methods' => ['GET'],
-// 	'callback' => [$namespace . '\ServicesController', 'all'],
-    'callback' => function ( $data) {
-        
-        $base_path  =   '/convoworks/services/'.$data['serviceId'].'/media/'.$data['mediaId'];
-        $json       =   json_decode( file_get_contents( wp_upload_dir()['basedir'].$base_path.'.json'), true);
-        $ext        =   $json['ext'];
-        
-        $response = new WP_REST_Response();
-        $response->set_status( 301);
-        
-        $response->header( 'Location', wp_upload_dir()['baseurl'].$base_path.'.'.$ext);
-        return $response;
-    },
-	'permission_callback' => function ($request) {
-		return true;
-	},
-]);
-// end media
-
 // public routes catch all
 register_rest_route('convo/v1', '/public/(?P<serviceId>[\S]+)', [
 	'methods' => ['GET', 'POST', 'PUT', 'DELETE'],
 	'callback' => [$namespace . '\ServicesController', 'publicRoutes'],
+	'permission_callback' => function ($request) {
+		return true;
+	},
+]);
+
+// Handle service-media differently
+register_rest_route('convo/v1', '/public/service-media/(?P<serviceId>[\S]+)/(?P<mediaId>[\S]+)', [
+	'methods' => ['GET'],
+	'callback' => [$namespace . '\ServicesController', 'mediaRoute'],
 	'permission_callback' => function ($request) {
 		return true;
 	},
--- src/Convo/Http/Api/ServicesController.php
+++ src/Convo/Http/Api/ServicesController.php
@@ -103,6 +103,63 @@ class ServicesController extends Controller
 		}
 	}
 
+	public static function mediaRoute(WP_REST_Request $request)
+	{
+		$route = $request->get_route();
+
+		$route = str_replace('public/', '', $route);
+
+		$uri = new Uri( CONVOWP_URL . '/wp-json' . $route);
+
+		$container = \Convo\Providers\ConvoWPPlugin::getPublicDiContainer();
+
+		/** @var \Psr\Log\LoggerInterface $logger */
+		$logger         =   $container->get('logger');
+
+		$adminRestApi = new PublicRestApi($logger, $container);
+
+		// loading WP user
+		$wpUser = wp_get_current_user();
+
+		$user =	new AdminUser($wpUser);
+
+		$middlewares    =   require_once(CONVOWP_LIB_COMMON_PATH . 'middlewares-client.php');
+		$app            =   new \Convo\Core\Util\RestApp($logger, $container, $adminRestApi, $middlewares);
+
+		$newRequest = Request::from_wp_request($request)
+		                     ->withUri($uri)
+		                     ->withParsedBody(json_decode($request->get_body(), true))
+			                 ->withQueryParams($request->get_params())
+		                     ->withAttribute( IAdminUser::class, $user);
+		try {
+			$response       =   $app->handle($newRequest);
+
+			// we need to redirect
+			if ($response->getStatusCode() === 302) {
+				$redirectTo = $response->getHeader('Location');
+				if (isset($redirectTo[0])) {
+					wp_redirect($redirectTo[0], 302);
+					die();
+				}
+			}
+
+			if ($response->getStatusCode() !== 200) {
+				return static::apiErrorResponse(json_decode($response->getBody()->getContents()), $response->getStatusCode());
+			}
+
+			$headers = $response->getHeaders();
+
+			foreach ($headers as $header => $values)
+			{
+				header("$header: " . implode("; ", $values), true, 200);
+			}
+
+			exit($response->getBody()->getContents());			
+		} catch (\Convo\Core\Rest\NotAuthenticatedException $e) {
+			return static::apiResponse(['message' => '403 User Not authorized'], 403);
+		}
+	}
+
 	public static function specialRoutes(WP_REST_Request $request)
 	{
 		$route = $request->get_route();
