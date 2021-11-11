51d8c0b065605c69adaab734da1f0042e3313d49
Marko Klobučar Ledinšćak <mrqoqlobo@gmail.com>
2021-11-11 22:50:48 +0100

    Fix wrong URL passed in constructor

--- lib/common/di-wp.php
+++ lib/common/di-wp.php
@@ -92,7 +92,7 @@ return [
         DI\get('httpFactory')
     ),
     'platformPublisherFactory' => DI\create('\Convo\Core\Publish\PlatformPublisherFactory')->constructor(
-        CONVO_BASE_URL,
+        CONVO_PUBLIC_REST_BASE_URL,
         DI\get('logger'),
         DI\get('convoServiceFactory'),
         DI\get('convoServiceDataProvider'),
