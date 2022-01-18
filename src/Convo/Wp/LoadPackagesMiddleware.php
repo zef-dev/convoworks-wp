8e77ee28731d3d39a07e89743e6f731aeca640e2
Marko Klobučar Ledinšćak <mrqoqlobo@gmail.com>
2022-01-18 12:52:07 +0100

    Fix package loader

--- src/Convo/Wp/LoadPackagesMiddleware.php
+++ src/Convo/Wp/LoadPackagesMiddleware.php
@@ -86,10 +86,6 @@ class LoadPackagesMiddleware implements \Psr\Http\Server\MiddlewareInterface
         $text->setLogger($this->_logger);
         $this->_packageProviderFactory->registerPackage($text);
 
-		$simpleAppointments = new ClassPackageDescriptor('\Convo\Pckg\Appointments\AppointmentsPackageDefinition', $this->_container);
-		$simpleAppointments->setLogger($this->_logger);
-		$this->_packageProviderFactory->registerPackage($simpleAppointments);
-
 		$convoAppointments = new FunctionPackageDescriptor('\Convo\Pckg\Appointments\AppointmentsPackageDefinition', function() {
 			return new \Convo\Pckg\Appointments\AppointmentsPackageDefinition( $this->_logger, $this->_container->get('alexaSettingsApi'));
 		});
