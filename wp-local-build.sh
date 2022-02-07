f75aebdcd8c70e644065a288fa2a8007ac8775d1
Marko Klobučar Ledinšćak <mrqoqlobo@gmail.com>
2022-02-07 13:19:17 +0100

    More info during build process

--- wp-local-build.sh
+++ wp-local-build.sh
@@ -1,29 +1,30 @@
 #!/bin/bash
 
+debug() {
+    echo "$(tput bold; tput setaf 4)[$(date +%H:%M:%S)]$(tput sgr 0) $1"
+}
+
 START=$(date +%s)
 
+debug "Cleaning pre-existing .workspace directory if any"
 rm -rf ./.workspace
+
+debug "Creating new .workspace directory"
 mkdir ./.workspace
+
+debug "Copying required files from root"
 cp ./composer-dev.json ./package.json ./gulpfile.js ./webpack.config.wp.js ./fix-autoloader.php ./scoper.inc.dev.php ./convo-plugin.php ./readme.txt ./.workspace
 cp -r ./app ./assets ./freemius ./lib ./public ./resources ./routes ./src ./webpack ./env ./.workspace
-cd .workspace
 
-# echo "$(tput setaf 5; tput setab 7)Removing /vendor$(tput sgr 0)"
-# rm -rf ./vendor
-# RMVENDOR=$!
-# wait $RMVENDOR
+debug "Moving into .workspace"
+cd .workspace || exit 1
 
 if [ ! -f "./composer-dev.json" ]; then
-    echo "You do not have a composer-dev.json file. Going to copy original."
+    debug "You do not have a composer-dev.json file. Going to copy original."
     cp ../composer.json ./composer-dev.json
 fi
 
-# echo "$(tput setaf 5; tput setab 7)Removing composer-dev.lock$(tput sgr 0)"
-# rm -rf ./composer-dev.lock
-# RMLOCK=$!
-# wait $RMLOCK
-
-echo "$(tput setaf 5; tput setab 7)Setting the COMPOSER envvar to composer-dev.json$(tput sgr 0)"
+debug "Running composer update"
 export COMPOSER=composer-dev.json
 composer update
 CUPDATE=$!
@@ -32,81 +33,86 @@ CUPDATE_WAIT_RES=$?
 export COMPOSER=composer.json
 
 if [ $CUPDATE_WAIT_RES -ne 0 ]; then
-    exit "composer update failed"
+    >&2 echo "composer update failed"
+    return $CUPDATE_WAIT_RES
 fi
 
-# echo "$(tput setaf 5; tput setab 7)Removing ./yarn.lock$(tput sgr 0)"
-# rm -rf ./yarn.lock
-# RMYARNLOCK=$!
-# wait $RMYARNLOCK
-
-echo "$(tput setaf 5; tput setab 7)Updating yarn.lock$(tput sgr 0)"
+debug "Updating yarn.lock"
 echo -e "graceful-fs@^4.2.2:\n  version \"4.2.2\"\n  resolved \"https://registry.yarnpkg.com/graceful-fs/-/graceful-fs-4.2.2.tgz#6f0952605d0140c1cfdb138ed005775b92d67b02\"\n  integrity sha512-IItsdsea19BoLC7ELy13q1iJFNmd7ofZH5+X/pJr90/nRoPEX0DJo1dHDbgtYWOhJhcCgMDTOw84RZ72q6lB+Q==" > yarn.lock
 
-# echo "$(tput setaf 5; tput setab 7)Removing ./node_modules$(tput sgr 0)"
-# rm -rf ./node_modules
-# RMNODEMODULES=$!
-# wait $RMNODEMODULES
-
-echo "$(tput setaf 5; tput setab 7)Running yarn$(tput sgr 0)"
+debug "Running yarn"
 yarn install
 YARNINSTALL=$!
 wait $YARNINSTALL
 
-echo "$(tput setaf 5; tput setab 7)Running yarn build:wp$(tput sgr 0)"
+# echo "$(tput setaf 5; tput setab 7)Running yarn build:wp$(tput sgr 0)"
+debug "Running yarn build:wp"
 yarn build:wp
 YARNBUILD=$!
 wait $YARNBUILD
 
-echo "$(tput setaf 5; tput setab 7)Copying files to public/assets/js$(tput sgr 0)"
+# echo "$(tput setaf 5; tput setab 7)Copying files to public/assets/js$(tput sgr 0)"
+debug "Copying files to public/assets/js"
 cp dist/www/main.js dist/www/vendor.js public/assets/js
 
-echo "$(tput setaf 5; tput setab 7)Fixing line endings$(tput sgr 0)"
+# echo "$(tput setaf 5; tput setab 7)Fixing line endings$(tput sgr 0)"
+debug "Fixing line endings"
 yarn run gulp fixLineEndings
 GULPFLE=$!
 wait $GULPFLE
 
+debug "Going to increase RC candidate"
 yarn run gulp bumpRcVersion
 GVERSION=$!
 wait $GVERSION
 
-# rm -rf build/
-
-echo "$(tput setaf 5; tput setab 7)Scoping PHP files$(tput sgr 0)"
+debug "Scoping PHP files"
 yes "yes" | php-scoper add-prefix --config scoper.inc.dev.php
 SCOPE=$!
 wait $SCOPE
 
-echo "$(tput setaf 5; tput setab 7)Moving into build dir$(tput sgr 0)"
-cd build
+debug "Moving into build directory"
+cd build || return 1
 
-echo "$(tput setaf 5; tput setab 7)Renaming composer files$(tput sgr 0)"
+debug "Renaming composer files"
 mv composer-dev.json composer.json
 mv composer-dev.lock composer.lock
+
+debug "Dumping composer autoloaders"
 composer dump-autoload
 
+debug "Fixing required autoload file in convo-plugin.php"
 sed -i -e "s/require_once __DIR__.'\/vendor\/autoload.php';/require_once __DIR__.'\/vendor\/scoper-autoload.php';/g" ./convo-plugin.php
 
+debug "Removing unnecessary composer files"
 rm composer.json
 rm composer.lock
 
+debug "Moving out of build directory"
 cd ../
 
+debug "Running autoloader fix script"
 php fix-autoloader.php
 
-echo "$(tput setaf 5; tput setab 7)Copying files from build to dist/convoworks-wp$(tput sgr 0)"
+debug "Copying files from build to dist/convoworks-wp"
 yes "y" | cp -rf build/* dist/convoworks-wp/
 
-echo "$(tput setaf 5; tput setab 7)Zipping files$(tput sgr 0)"
+debug "Zipping built files"
 yarn run gulp zip
 
+debug "Moving to root dir"
 cd ../
+
+debug "Cleaning dist directory and copying files from .workspace/dist"
 rm -rf ./dist
 cp -r .workspace/dist ./
+
+debug "Copying package.json and convo-plugin.php from .workspace"
 yes "y" | cp -fR .workspace/package.json .workspace/convo-plugin.php ./
 
+debug "Build done, cleaning .workspace"
 rm -rf ./.workspace
 
 END=$(date +%s)
 
-echo "Total execution time was $(($END - $START)) seconds."
+debug "Total execution time was $(($END - $START)) seconds."
