b1c7c2e8433d3fab0e265be06301e5b191d94f35
Tihomir Dmitrovic <tole.car@gmail.com>
2022-10-17 09:05:39 +0200

    Update music player templates

--- src/Convo/Wp/Pckg/WpCore/WpPostsPackageDefinition.php
+++ src/Convo/Wp/Pckg/WpCore/WpPostsPackageDefinition.php
@@ -36,8 +36,8 @@ class WpPostsPackageDefinition extends AbstractPackageDefinition
         parent::__construct($logger, self::NAMESPACE, __DIR__);
 
 // 	    $this->addTemplate( $this->_loadFile(__DIR__ . '/convo-account-linking.template.json'));
-	    $this->addTemplate( $this->_loadFile(__DIR__ . '/searchable-music-player.template.json'));
-	    $this->addTemplate( $this->_loadFile(__DIR__ . '/loop-music-player.template.json'));
+	    $this->addTemplate( $this->_loadFile(__DIR__ . '/searchable-player.template.json'));
+	    $this->addTemplate( $this->_loadFile(__DIR__ . '/loop-player.template.json'));
 	    $this->addTemplate( $this->_loadFile(__DIR__ . '/loop-example.template.json'));
 	    $this->addTemplate( $this->_loadFile(__DIR__ . '/loop-list.template.json'));
     }
