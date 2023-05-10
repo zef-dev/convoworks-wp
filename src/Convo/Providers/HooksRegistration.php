5aa3fca11d6f99cee5af44e4935fde02aa150fc4
Tihomir Dmitrovic <tole.car@gmail.com>
2023-05-10 09:00:19 +0200

    Reuse load service

--- src/Convo/Providers/HooksRegistration.php
+++ src/Convo/Providers/HooksRegistration.php
@@ -23,87 +23,65 @@ class HooksRegistration
             } else {
                 throw new \Exception( 'Unexpected hook type ['.$hook['hook_type'].']');
             }
-            
         }
     }
     
     private function _registerFilterHook( $hook)
     {
-        add_filter( $hook['hook'], function () use ( $hook) {
+        add_filter( $hook['hook'], function () use ( $hook) 
+        {
+            $args           =   func_get_args();
+            $request_id     =   StrUtil::uuidV4();
             
-            $di     =   ConvoWPPlugin::getPublicDiContainer();
-            ConvoWPPlugin::loadPackages( $di);
-            $args   =   func_get_args();
-            
-            $owner		        =	new RestSystemUser();
-            $request_id         =   StrUtil::uuidV4();
-            
-            $convoServiceFactory =  $di->get( 'convoServiceFactory');
-            $convoServiceParamsFactory = $di->get( 'convoServiceParamsFactory');
-            /* @var \Convo\Core\Factory\ConvoServiceFactory $convoServiceFactory */
-            /* @var \Convo\Core\Params\IServiceParamsFactory $convoServiceParamsFactory */
-            
-//             $convoServiceDataProvider = $di->get( 'convoServiceDataProvider');
-            
-//             $version_id			=	$convoServiceFactory->getVariantVersion( 
-//                 $owner, $hook['service_id'], WpHooksCommandRequest::PLATFORM_ID, $hook['variant']);
-//             $platform_config	=	$convoServiceDataProvider->getServicePlatformConfig( $owner, $hook['service_id'], $version_id);
-            
-//             $this->_logger->debug( 'Got config ['.print_r( $platform_config, true).']');
-            
-//             if ( !isset( $platform_config[WpHooksCommandRequest::PLATFORM_ID])) {
-//                 throw new \Convo\Core\Rest\InvalidRequestException( 'Service ['.$hook['service_id'].'] version ['.$version_id.'] is not enabled for platform ['.'convo_chat'.']');
-//             }
-            
-            $text_request		=	new WpHooksCommandRequest(
+            $text_request   =    new WpHooksCommandRequest(
                 $hook['service_id'], 'system', 'wo', null, $request_id, $hook['hook'], $args, null);
-            $text_response		=	new WpHooksCommandResponse( $text_request);
+            $text_response  =    new WpHooksCommandResponse( $text_request);
             
-            $service			=	$convoServiceFactory->getService( 
-                $owner, $hook['service_id'], $hook['version'], $convoServiceParamsFactory);
+            $service        =   $this->_getLoadedService( $hook['service_id'], $hook['version']);
             $service->run( $text_request, $text_response);
             
             return $text_response->getFilterResponse();
+            
         }, $hook['priority'], $hook['accepted_args']);
     }
     
     private function _registerActionHook( $hook) 
     {
-        add_action( $hook['hook'], function () use ( $hook) {
-            $di     =   ConvoWPPlugin::getPublicDiContainer();
-            ConvoWPPlugin::loadPackages( $di);
-            $args   =   func_get_args();
+        add_action( $hook['hook'], function () use ( $hook) 
+        {
+            $args           =   func_get_args();
+            $request_id     =   StrUtil::uuidV4();
             
-            $owner		        =	new RestSystemUser();
-            $request_id         =   StrUtil::uuidV4();
-            
-            $convoServiceFactory =  $di->get( 'convoServiceFactory');
-            $convoServiceParamsFactory = $di->get( 'convoServiceParamsFactory');
-            
-            /* @var \Convo\Core\Factory\ConvoServiceFactory $convoServiceFactory */
-            /* @var \Convo\Core\Params\IServiceParamsFactory $convoServiceParamsFactory */
-            
-//             $convoServiceDataProvider = $di->get( 'convoServiceDataProvider');
-            
-            //             $version_id			=	$convoServiceFactory->getVariantVersion(
-            //                 $owner, $hook['service_id'], WpHooksCommandRequest::PLATFORM_ID, $hook['variant']);
-            //             $platform_config	=	$convoServiceDataProvider->getServicePlatformConfig( $owner, $hook['service_id'], $version_id);
-            
-            //             $this->_logger->debug( 'Got config ['.print_r( $platform_config, true).']');
-            
-            //             if ( !isset( $platform_config[WpHooksCommandRequest::PLATFORM_ID])) {
-            //                 throw new \Convo\Core\Rest\InvalidRequestException( 'Service ['.$hook['service_id'].'] version ['.$version_id.'] is not enabled for platform ['.'convo_chat'.']');
-            //             }
-            
-            $text_request		=	new WpHooksCommandRequest(
+            $text_request   =   new WpHooksCommandRequest(
                 $hook['service_id'], 'system', 'wo', null, $request_id, $hook['hook'], $args, null);
-            $text_response		=	new DefaultTextCommandResponse();
+            $text_response  =   new DefaultTextCommandResponse();
             
-            $service			=	$convoServiceFactory->getService( 
-                $owner, $hook['service_id'], $hook['version'], $convoServiceParamsFactory);
+            $service        =   $this->_getLoadedService( $hook['service_id'], $hook['version']);
             $service->run( $text_request, $text_response);
             
         }, $hook['priority'], $hook['accepted_args']);
+    }
+    
+    /**
+     * @param string $serviceId
+     * @param string $versionId
+     * @return \Convo\Core\ConvoServiceInstance
+     */
+    private function _getLoadedService( $serviceId, $versionId)
+    {
+        /* @var \Convo\Core\Factory\ConvoServiceFactory $convoServiceFactory */
+        /* @var \Convo\Core\Params\IServiceParamsFactory $convoServiceParamsFactory */
+        
+        $owner  =   new RestSystemUser();
+        $di     =   ConvoWPPlugin::getPublicDiContainer();
+        $convoServiceFactory        =   $di->get( 'convoServiceFactory');
+        $convoServiceParamsFactory  =   $di->get( 'convoServiceParamsFactory');
+        
+        ConvoWPPlugin::loadPackages( $di);
+
+        $service    =   $convoServiceFactory->getService(
+            $owner, $serviceId, $versionId, $convoServiceParamsFactory);
+        return $service;
     }
     
     private function _getRequiredHooks()
