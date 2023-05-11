c468501dbf92ac5ff3e867c3608c335487adde25
Tihomir Dmitrovic <tole.car@gmail.com>
2023-05-11 10:59:23 +0200

    Use special role

--- src/Convo/Providers/HooksRegistration.php
+++ src/Convo/Providers/HooksRegistration.php
@@ -7,12 +7,14 @@ use Convo\Core\Util\StrUtil;
 use Convo\Wp\Pckg\WpHooks\WpHooksCommandRequest;
 use Convo\Wp\Pckg\WpHooks\WpHooksCommandResponse;
 use Convo\Core\Adapters\ConvoChat\DefaultTextCommandResponse;
+use Convo\Wp\Pckg\WpHooks\WpHooksPlatform;
 
 class HooksRegistration
 {
     const WP_HOOKS_OPTION = 'convoworks_hooks_handler';
     
     private $_loadedServices = [];
+    private $_loadedConfigs = [];
     
     public function register()
     {
@@ -37,8 +39,9 @@ class HooksRegistration
             $request_id     =   StrUtil::uuidV4();
             
             try {
+                $config         =   $this->_getLoadedServiceConfig( $hook['service_id'], $hook['version']);
                 $text_request   =    new WpHooksCommandRequest(
-                    $hook['service_id'], 'system', 'wo', null, $request_id, $hook['hook'], $args);
+                    $hook['service_id'], 'system', 'wo', null, $request_id, $hook['hook'], $args, $config['special_role']);
                 $text_response  =    new WpHooksCommandResponse( $text_request);
                 
                 $service        =   $this->_getLoadedService( $hook['service_id'], $hook['version']);
@@ -60,8 +63,9 @@ class HooksRegistration
             $request_id     =   StrUtil::uuidV4();
             
             try {
+                $config         =   $this->_getLoadedServiceConfig( $hook['service_id'], $hook['version']);
                 $text_request   =   new WpHooksCommandRequest(
-                    $hook['service_id'], 'system', 'wo', null, $request_id, $hook['hook'], $args);
+                    $hook['service_id'], 'system', 'wo', null, $request_id, $hook['hook'], $args, $config['special_role']);
                 $text_response  =   new DefaultTextCommandResponse();
                 
                 $service        =   $this->_getLoadedService( $hook['service_id'], $hook['version']);
@@ -98,6 +102,31 @@ class HooksRegistration
         }
         
         return $this->_loadedServices[$key];
+    }
+    
+    /**
+     * @param string $serviceId
+     * @param string $versionId
+     * @return array
+     */
+    private function _getLoadedServiceConfig( $serviceId, $versionId)
+    {
+        $key = $serviceId.'_'.$versionId;
+        
+        if ( !isset( $this->_loadedConfigs[$key]))
+        {
+            /* @var \Convo\Core\IServiceDataProvider $convoServiceDataProvider */
+            
+            $owner  =   new RestSystemUser();
+            $di     =   ConvoWPPlugin::getPublicDiContainer();
+            $convoServiceDataProvider      =   $di->get( 'convoServiceDataProvider');
+            
+            $config    =   $convoServiceDataProvider->getServicePlatformConfig(
+                $owner, $serviceId, $versionId);
+            $this->_loadedConfigs[$key] = $config[WpHooksPlatform::PLATFORM_ID];
+        }
+        
+        return $this->_loadedConfigs[$key];
     }
     
     public static function getRequiredHooks()
--- src/Convo/Wp/Pckg/WpHooks/WpHooksCommandRequest.php
+++ src/Convo/Wp/Pckg/WpHooks/WpHooksCommandRequest.php
@@ -4,46 +4,47 @@ namespace Convo\Wp\Pckg\WpHooks;
 
 use Convo\Core\Adapters\ConvoChat\DefaultTextCommandRequest;
 use Convo\Core\DataItemNotFoundException;
+use Convo\Core\Workflow\ISpecialRoleRequest;
 
-class WpHooksCommandRequest extends DefaultTextCommandRequest
+class WpHooksCommandRequest extends DefaultTextCommandRequest implements ISpecialRoleRequest
 {
+    private $_specialRole;
     private $_hook;
     private $_arguments;
     
-    public function __construct( $serviceId, $installationId, $deviceId, $sessionId, $requestId, $hook, $arguments)
-	{
-	    parent::__construct( $serviceId, $installationId, $sessionId, $requestId, null, true, false, WpHooksPlatform::PLATFORM_ID, []);
-	    
-	    $this->_hook = $hook;
-	    $this->_arguments = $arguments;
-	}
-	
-	public function isEmpty() {
-	    return false;
-	}
+    public function __construct( $serviceId, $installationId, $deviceId, $sessionId, $requestId, $hook, $arguments, $specialRole)
+    {
+        parent::__construct( $serviceId, $installationId, $sessionId, $requestId, $hook, true, false, WpHooksPlatform::PLATFORM_ID, $arguments);
+        
+        $this->_specialRole    =   $specialRole;
+        $this->_hook           =   $hook;
+        $this->_arguments      =   $arguments;
+    }
     
-	
-	public function getHook()
-	{
-	    return $this->_hook;
-	}
-	
-	public function getArguments()
-	{
-	    return $this->_arguments;
-	}
-	
-	public function getArgument( $index)
-	{
-	    if ( !isset( $this->_arguments[$index])) {
-	        throw new DataItemNotFoundException( 'Hook ['.$this->_hook.'] argument not defined ['.$index.']');
-	    }
-	    return $this->_arguments[$index];
-	}
-	
+    public function getSpecialRole()
+    {
+        return $this->_specialRole;
+    }
+    
+    public function getHook()
+    {
+        return $this->_hook;
+    }
+    
+    public function getArguments()
+    {
+        return $this->_arguments;
+    }
+    
+    public function getArgument( $index)
+    {
+        if ( !isset( $this->_arguments[$index])) {
+            throw new DataItemNotFoundException( 'Hook ['.$this->_hook.'] argument not defined ['.$index.']');
+        }
+        return $this->_arguments[$index];
+    }
     
     public function __toString() {
-        return parent::__toString().'['.$this->_hook.']';
+        return parent::__toString().'['.$this->_hook.']['.$this->_specialRole.']';
     }
-	
 }
