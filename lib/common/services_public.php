d6817941d90a9ed874f0114e84a2dd039fb16ec1
Tihomir Dmitrovic <tole.car@gmail.com>
2024-09-02 21:45:57 +0200

    Standardize param names

--- lib/common/services_public.php
+++ lib/common/services_public.php
@@ -33,9 +33,9 @@ $CONVO_SHOULD_DUMP_REQUESTS_AND_RESPONSES = defined('CONVO_SHOULD_DUMP_REQUESTS_
 $containerBuilder->setParameter('convo.log_level', defined('CONVO_LOG_LEVEL') ? CONVO_LOG_LEVEL : 'info');
 $containerBuilder->setParameter('convo.log_path', defined('CONVO_LOG_PATH') ? CONVO_LOG_PATH : null);
 $containerBuilder->setParameter('convo.log_filename', defined('CONVO_LOG_FILENAME') ? CONVO_LOG_FILENAME : 'debug.log');
-$containerBuilder->setParameter('convo.log_level_public', defined('CONVO_LOG_LEVEL_PUBLIC') ? CONVO_LOG_LEVEL_PUBLIC : '%convo.log_level%');
-$containerBuilder->setParameter('convo.log_path_public', defined('CONVO_LOG_PATH_PUBLIC') ? CONVO_LOG_PATH_PUBLIC : '%convo.log_path%');
-$containerBuilder->setParameter('convo.log_filename_public', defined('CONVO_LOG_FILENAME_PUBLIC') ? CONVO_LOG_FILENAME_PUBLIC : '%convo.log_filename%');
+$containerBuilder->setParameter('convo.log_level', defined('CONVO_LOG_LEVEL_PUBLIC') ? CONVO_LOG_LEVEL_PUBLIC : '%convo.log_level%');
+$containerBuilder->setParameter('convo.log_path', defined('CONVO_LOG_PATH_PUBLIC') ? CONVO_LOG_PATH_PUBLIC : '%convo.log_path%');
+$containerBuilder->setParameter('convo.log_filename', defined('CONVO_LOG_FILENAME_PUBLIC') ? CONVO_LOG_FILENAME_PUBLIC : '%convo.log_filename%');
 
 // Register the public logger
 $containerBuilder->register('logger', Logger::class)
@@ -45,16 +45,16 @@ $containerBuilder->register('logger', Logger::class)
 // Register the logger handler factory
 $containerBuilder->register('logger_handler_factory')
     ->setFactory(['logger_handler_factory', 'createHandler'])
-    ->addArgument('%convo.log_path_public%')
-    ->addArgument('%convo.log_filename_public%')
-    ->addArgument('%convo.log_level_public%');
+    ->addArgument('%convo.log_path%')
+    ->addArgument('%convo.log_filename%')
+    ->addArgument('%convo.log_level%');
 
 // Register the logger handler using the factory
 $containerBuilder->register('logger_handler', StreamHandler::class)
     ->setFactory([new Reference('logger_handler_factory'), 'createHandler'])
-    ->addArgument('%convo.log_path_public%')
-    ->addArgument('%convo.log_filename_public%')
-    ->addArgument('%convo.log_level_public%')
+    ->addArgument('%convo.log_path%')
+    ->addArgument('%convo.log_filename%')
+    ->addArgument('%convo.log_level%')
     ->addMethodCall('setFormatter', [new Reference('logger_formatter')]);
 
 // Register the logger formatter
