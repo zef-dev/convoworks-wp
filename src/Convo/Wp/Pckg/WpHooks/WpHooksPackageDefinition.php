bde36681b69981e138ab93010e429ec457e8867f
Tihomir Dmitrovic <tole.car@gmail.com>
2025-02-23 20:52:30 +0100

    Register mnore scheduler functions

--- src/Convo/Wp/Pckg/WpHooks/WpHooksPackageDefinition.php
+++ src/Convo/Wp/Pckg/WpHooks/WpHooksPackageDefinition.php
@@ -1,4 +1,6 @@
-<?php declare(strict_types=1);
+<?php
+
+declare(strict_types=1);
 
 namespace Convo\Wp\Pckg\WpHooks;
 
@@ -10,25 +12,25 @@ use Convo\Core\Expression\ExpressionFunction;
 class WpHooksPackageDefinition extends AbstractPackageDefinition implements IPlatformProvider
 {
     const NAMESPACE    =    'convo-wp-hooks';
-    
+
     /**
      * @var WpHooksPlatform
      */
     private $_platform;
-    
-    public function __construct( \Psr\Log\LoggerInterface $logger, $platform) 
+
+    public function __construct(\Psr\Log\LoggerInterface $logger, $platform)
     {
         $this->_platform = $platform;
-        
-        parent::__construct( $logger, self::NAMESPACE, __DIR__);
-        
-        $this->registerTemplate( __DIR__ .'/wordpress-hooks-project.template.json');
+
+        parent::__construct($logger, self::NAMESPACE, __DIR__);
+
+        $this->registerTemplate(__DIR__ . '/wordpress-hooks-project.template.json');
     }
-    
+
     public function getFunctions()
     {
         $functions = [];
-        
+
         $functions[] = new ExpressionFunction(
             'register_post_type',
             function ($post_type, $typeargs = null) {
@@ -38,29 +40,29 @@ class WpHooksPackageDefinition extends AbstractPackageDefinition implements IPla
                 return register_post_type($post_type, $typeargs);
             }
         );
-        
+
         // https://developer.wordpress.org/reference/functions/is_home/
         $functions[] = new ExpressionFunction(
             'is_home',
             function () {
-                return sprintf( 'is_home()');
+                return sprintf('is_home()');
             },
-            function( $args) {
+            function ($args) {
                 return is_home();
             }
         );
-        
+
         // https://developer.wordpress.org/reference/functions/is_admin/
         $functions[] = new ExpressionFunction(
             'is_admin',
             function () {
-                return sprintf( 'is_admin()');
+                return sprintf('is_admin()');
             },
-            function( $args) {
+            function ($args) {
                 return is_admin();
             }
         );
-        
+
         // https://developer.wordpress.org/reference/functions/register_sidebar/
         $functions[] = new ExpressionFunction(
             'register_sidebar',
@@ -71,7 +73,7 @@ class WpHooksPackageDefinition extends AbstractPackageDefinition implements IPla
                 return register_sidebar($wpArgs);
             }
         );
-        
+
         // https://developer.wordpress.org/reference/functions/remove_filter/
         $functions[] = new ExpressionFunction(
             'remove_filter',
@@ -82,7 +84,7 @@ class WpHooksPackageDefinition extends AbstractPackageDefinition implements IPla
                 return remove_filter($hook, $callback, $priority);
             }
         );
-        
+
         // https://developer.wordpress.org/reference/functions/remove_action/
         $functions[] = new ExpressionFunction(
             'remove_action',
@@ -93,7 +95,7 @@ class WpHooksPackageDefinition extends AbstractPackageDefinition implements IPla
                 return remove_action($hook, $callback, $priority);
             }
         );
-        
+
         // https://developer.wordpress.org/reference/functions/wp_redirect/
         $functions[] = new ExpressionFunction(
             'wp_redirect',
@@ -104,7 +106,7 @@ class WpHooksPackageDefinition extends AbstractPackageDefinition implements IPla
                 return wp_redirect($location, $status, $redirectBy);
             }
         );
-        
+
         // https://developer.wordpress.org/reference/functions/do_shortcode/
         $functions[] = new ExpressionFunction(
             'do_shortcode',
@@ -115,7 +117,7 @@ class WpHooksPackageDefinition extends AbstractPackageDefinition implements IPla
                 return do_shortcode($content, $ignoreHtml);
             }
         );
-        
+
         // https://developer.wordpress.org/reference/functions/wp_next_scheduled/
         $functions[] = new ExpressionFunction(
             'wp_next_scheduled',
@@ -126,7 +128,7 @@ class WpHooksPackageDefinition extends AbstractPackageDefinition implements IPla
                 return wp_next_scheduled($hook, $hookArgs);
             }
         );
-        
+
         // https://developer.wordpress.org/reference/functions/wp_schedule_event/
         $functions[] = new ExpressionFunction(
             'wp_schedule_event',
@@ -137,7 +139,7 @@ class WpHooksPackageDefinition extends AbstractPackageDefinition implements IPla
                 return wp_schedule_event($timestamp, $recurrence, $hook, $hookArgs, $wpError);
             }
         );
-        
+
         // https://developer.wordpress.org/reference/functions/wp_unschedule_event/
         $functions[] = new ExpressionFunction(
             'wp_unschedule_event',
@@ -148,8 +150,8 @@ class WpHooksPackageDefinition extends AbstractPackageDefinition implements IPla
                 return wp_unschedule_event($timestamp, $hook, $args);
             }
         );
-    
-        
+
+
         // https://developer.wordpress.org/reference/functions/add_rewrite_rule/
         $functions[] = new ExpressionFunction(
             'add_rewrite_rule',
@@ -160,10 +162,54 @@ class WpHooksPackageDefinition extends AbstractPackageDefinition implements IPla
                 return add_rewrite_rule($regex, $redirect, $after);
             }
         );
-        
+
+        $functions[] = new ExpressionFunction(
+            'wp_schedule_single_event',
+            function ($timestamp, $hook, $args = []) {
+                return sprintf(
+                    'wp_schedule_single_event(%s, %s, %s)',
+                    var_export($timestamp, true),
+                    var_export($hook, true),
+                    var_export($args, true)
+                );
+            },
+            function ($args, $timestamp, $hook, $argsArray = []) {
+                return wp_schedule_single_event($timestamp, $hook, $argsArray);
+            }
+        );
+
+        $functions[] = new ExpressionFunction(
+            'wp_clear_scheduled_hook',
+            function ($hook, $args = []) {
+                return sprintf(
+                    'wp_clear_scheduled_hook(%s, %s)',
+                    var_export($hook, true),
+                    var_export($args, true)
+                );
+            },
+            function ($args, $hook, $argsArray = []) {
+                return wp_clear_scheduled_hook($hook, $argsArray);
+            }
+        );
+
+        $functions[] = new ExpressionFunction(
+            'wp_get_scheduled_event',
+            function ($hook, $args = []) {
+                return sprintf(
+                    'wp_get_scheduled_event(%s, %s)',
+                    var_export($hook, true),
+                    var_export($args, true)
+                );
+            },
+            function ($args, $hook, $argsArray = []) {
+                return wp_get_scheduled_event($hook, $argsArray);
+            }
+        );
+
+
         return $functions;
     }
-    
+
     protected function _initDefintions()
     {
         return [
@@ -184,8 +230,8 @@ class WpHooksPackageDefinition extends AbstractPackageDefinition implements IPla
                     '_preview_angular' => [
                         'type' => 'html',
                         'template' => '<div class="code"><span class="statement">WP FILTER RESULT</span>' .
-                        ' {{component.properties.return_value}}' .
-                        '</div>'
+                            ' {{component.properties.return_value}}' .
+                            '</div>'
                     ],
                     '_workflow' => 'read',
                     '_help' =>  [
@@ -227,9 +273,9 @@ class WpHooksPackageDefinition extends AbstractPackageDefinition implements IPla
                     '_preview_angular' => [
                         'type' => 'html',
                         'template' => '<div class="code"><span class="statement">WP ERROR</span>' .
-                        ' {{component.properties.code}}' .
-                        ' {{component.properties.message}}' .
-                        '</div>'
+                            ' {{component.properties.code}}' .
+                            ' {{component.properties.message}}' .
+                            '</div>'
                     ],
                     '_workflow' => 'read',
                     '_help' =>  [
@@ -255,8 +301,8 @@ class WpHooksPackageDefinition extends AbstractPackageDefinition implements IPla
                     '_preview_angular' => [
                         'type' => 'html',
                         'template' => '<div class="code"><span class="statement">ECHO</span>' .
-                        ' {{component.properties.text}}' .
-                        '</div>'
+                            ' {{component.properties.text}}' .
+                            '</div>'
                     ],
                     '_workflow' => 'read',
                     '_help' =>  [
@@ -274,7 +320,7 @@ class WpHooksPackageDefinition extends AbstractPackageDefinition implements IPla
                     '_preview_angular' => [
                         'type' => 'html',
                         'template' => '<div class="code"><span class="statement">EXIT</span>' .
-                        '</div>'
+                            '</div>'
                     ],
                     '_workflow' => 'read',
                     '_help' =>  [
@@ -338,8 +384,8 @@ class WpHooksPackageDefinition extends AbstractPackageDefinition implements IPla
                     '_preview_angular' => [
                         'type' => 'html',
                         'template' => '<div class="code"><span class="statement">ADD {{component.properties.hookType}}</span>' .
-                        ' {{component.properties.hook}}' .
-                        '</div>'
+                            ' {{component.properties.hook}}' .
+                            '</div>'
                     ],
                     '_workflow' => 'process',
                     '_help' =>  [
@@ -350,24 +396,24 @@ class WpHooksPackageDefinition extends AbstractPackageDefinition implements IPla
             ),
         ];
     }
-    public function getPlatform( $platformId)
+    public function getPlatform($platformId)
     {
-        if ( strpos( $platformId, '.') === false) {
-            $search = self::NAMESPACE.'.'.$platformId;
+        if (strpos($platformId, '.') === false) {
+            $search = self::NAMESPACE . '.' . $platformId;
         } else {
             $search = $platformId;
         }
-        
-        $this->_logger->info( 'Searching for platform ['.$platformId.']['.$search.']');
-        $this->_logger->debug( 'Comparing to platform ['.$this->_platform->getPlatformId().']');
-        
-        if ( $search === $this->_platform->getPlatformId()) {
+
+        $this->_logger->info('Searching for platform [' . $platformId . '][' . $search . ']');
+        $this->_logger->debug('Comparing to platform [' . $this->_platform->getPlatformId() . ']');
+
+        if ($search === $this->_platform->getPlatformId()) {
             return $this->_platform;
         }
-        
-        throw new ComponentNotFoundException( 'Could not locate platform ['.$platformId.']['.$search.']');
+
+        throw new ComponentNotFoundException('Could not locate platform [' . $platformId . '][' . $search . ']');
     }
-    
+
     public function getRow()
     {
         $data = parent::getRow();
@@ -376,11 +422,10 @@ class WpHooksPackageDefinition extends AbstractPackageDefinition implements IPla
                 'name' => 'WordPress Hooks',
                 'description' => 'WordPress hooks configuration',
                 'route' => 'convoworks-editor-service.configuration-wp-hooks',
-                'icon_url' => CONVOWP_ASSETS_URL.'/images/wp-hooks-platform.jpg',
+                'icon_url' => CONVOWP_ASSETS_URL . '/images/wp-hooks-platform.jpg',
             ],
         ];
-        
+
         return $data;
     }
-
 }
