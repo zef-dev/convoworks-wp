1b90ca0c940323f21b1e3df9e8dcea6a90c683c8
Tihomir Dmitrovic <tole.car@gmail.com>
2025-02-23 21:08:07 +0100

    Fix formatting & rc version

--- convo-plugin.php
+++ convo-plugin.php
@@ -9,28 +9,28 @@
  * Plugin URI: https://convoworks.com
  * Update URI: https://wpdemo.convoworks.com/wp-content/uploads/deploy/convoworks-wp/info.json
  * Author: ZEF Development
- * Version: 0.22.41-RC01
+ * Version: 0.22.42-RC02
  * Author URI: https://zef.dev
  * Text Domain: convo-wp
  * Domain Path: /resources/lang
  */
 
-if ( ! defined( 'ABSPATH' ) ) {
-    die( 'You are not allowed to call this page directly.' );
+if (! defined('ABSPATH')) {
+    die('You are not allowed to call this page directly.');
 }
 
-if ( ! defined( 'CONVOWP_LOCAL' ) ) {
+if (! defined('CONVOWP_LOCAL')) {
     define('CONVOWP_LOCAL', false);
 }
 
 use Convo\Providers\ConvoWPPlugin;
 
-define('CONVOWP_VERSION', '0.22.41-RC01');
+define('CONVOWP_VERSION', '0.22.42-RC02');
 define('CONVOWP_PLUGIN_SLUG', plugin_basename(__FILE__));
 define('CONVOWP_FILE', __FILE__);
 define('CONVOWP_PATH', __DIR__);
-define('CONVOWP_URL' , plugin_dir_url(__FILE__));
-define('CONVOWP_ASSETS_PATH' , CONVOWP_PATH . '/public/assets/');
+define('CONVOWP_URL', plugin_dir_url(__FILE__));
+define('CONVOWP_ASSETS_PATH', CONVOWP_PATH . '/public/assets/');
 define('CONVOWP_ASSETS_URL', CONVOWP_URL . 'public/assets/');
 define('CONVOWP_RESOURCES_URL', CONVOWP_URL . 'resources/');
 define('CONVOWP_RESOURCES_PATH', CONVOWP_PATH . '/resources/');
@@ -43,32 +43,31 @@ define('CONVO_DB_VERSION', '1.0.12');
 define('CONVOWP_LIB_COMMON_PATH', CONVOWP_PATH . '/lib/common/');
 
 // CONVO RELATED
-define( 'CONVO_DATA_PATH', wp_upload_dir()['basedir'] . '/convoworks');
-define( 'CONVO_MEDIA_BASE_URL', wp_upload_dir()['baseurl'] . '/convoworks');
-define( 'CONVO_BASE_URL', site_url());
-define( 'CONVO_PUBLIC_REST_BASE_URL', CONVO_BASE_URL . '/wp-json/convo/v1/public');
-define( 'CONVO_UTIL_DISABLE_GZIP_ENCODING', false); // faster Rest responses, but can cause problemss in development and debuging
+define('CONVO_DATA_PATH', wp_upload_dir()['basedir'] . '/convoworks');
+define('CONVO_MEDIA_BASE_URL', wp_upload_dir()['baseurl'] . '/convoworks');
+define('CONVO_BASE_URL', site_url());
+define('CONVO_PUBLIC_REST_BASE_URL', CONVO_BASE_URL . '/wp-json/convo/v1/public');
+define('CONVO_UTIL_DISABLE_GZIP_ENCODING', false); // faster Rest responses, but can cause problemss in development and debuging
 
 // Initialize the plugin
 if (version_compare(PHP_VERSION, '7.2', ">=")) {
     add_filter('update_plugins_wpdemo.convoworks.com', 'convoworks_wp_check_for_updates', 10, 3);
 
-    error_log('ConvoWPPlugin INCLUDE after ['.timer_stop(0,6).'ms]');
+    error_log('ConvoWPPlugin INCLUDE after [' . timer_stop(0, 6) . 'ms]');
     // Add autoloader
-    if ( CONVOWP_LOCAL) {
-        require_once __DIR__.'/vendor/autoload.php';
+    if (CONVOWP_LOCAL) {
+        require_once __DIR__ . '/vendor/autoload.php';
     } else {
-        require_once __DIR__.'/vendor/scoper-autoload.php';
+        require_once __DIR__ . '/vendor/scoper-autoload.php';
     }
 
     $plugin = new ConvoWPPlugin();
     $plugin->init();
 
-    error_log('ConvoWPPlugin INIT DONE after ['.timer_stop(0,6).'ms]');
-
+    error_log('ConvoWPPlugin INIT DONE after [' . timer_stop(0, 6) . 'ms]');
 } else {
     if (is_admin()) {
-        add_action('all_admin_notices', function() {
+        add_action('all_admin_notices', function () {
             echo esc_html('<div class="error"><p>You need PHP v7.2+ to use the ConvoWp plugin. You currently have ' . PHP_VERSION . '</p></div>');
         });
     }
@@ -88,7 +87,7 @@ function convoworks_wp_check_for_updates($update, $plugin_data, $plugin_file)
 
     if (is_a($response, 'WP_Error')) {
         /** @var WP_Error $response */
-        error_log('Error updating plugin [Convoworks WP]: '.implode("\n", $response->get_error_messages()));
+        error_log('Error updating plugin [Convoworks WP]: ' . implode("\n", $response->get_error_messages()));
         return $update;
     }
 
@@ -100,17 +99,15 @@ function convoworks_wp_check_for_updates($update, $plugin_data, $plugin_file)
 
     if (!empty($custom_plugins_data[$plugin_file])) {
         return $custom_plugins_data[$plugin_file];
-    }
-    else {
+    } else {
         return $update;
     }
 }
 
 // Plugin activation and deactivation
 if (version_compare(PHP_VERSION, '7.2', ">=")) {
-    register_activation_hook( __FILE__, [ \Convo\Providers\PluginActivator::class, 'activate' ] );
-    register_deactivation_hook( __FILE__, [ \Convo\Providers\PluginActivator::class, 'deactivate' ] );
-    add_action( 'activated_plugin', [ \Convo\Providers\PluginActivator::class, 'afterActivate' ] );
-    add_action( 'deactivated_plugin', [ \Convo\Providers\PluginActivator::class, 'afterDeactivate' ] );
+    register_activation_hook(__FILE__, [\Convo\Providers\PluginActivator::class, 'activate']);
+    register_deactivation_hook(__FILE__, [\Convo\Providers\PluginActivator::class, 'deactivate']);
+    add_action('activated_plugin', [\Convo\Providers\PluginActivator::class, 'afterActivate']);
+    add_action('deactivated_plugin', [\Convo\Providers\PluginActivator::class, 'afterDeactivate']);
 }
-
