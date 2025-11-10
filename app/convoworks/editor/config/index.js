6522668e4423930ffab103793b5eb0c76c6114fa
Tihomir Dmitrovic <tole.car@gmail.com>
2025-11-10 22:54:28 +0100

    Remove df and messenger views

--- app/convoworks/editor/config/index.js
+++ app/convoworks/editor/config/index.js
@@ -3,9 +3,6 @@ import angular from 'angular';
 import configurationView from "./configuration-view.directive";
 import configAmazonEditor from './config-amazon-editor.directive';
 import configConvoChatEditor from './config-convo-chat-editor.directive';
-import configDialogflowEditor from './config-dialogflow-editor.directive';
-import configDialogflowEsEditor from './config-dialogflow-es-editor.directive';
-import configMessengerEditor from './config-messenger-editor.directive';
 import configViberEditor from './config-viber-editor.directive';
 import configServiceMetaEditor from './config-service-meta-editor.directive';
 
@@ -17,9 +14,6 @@ export default angular
     .directive('configurationView', configurationView)
     .directive('configAmazonEditor', configAmazonEditor)
     .directive('configConvoChatEditor', configConvoChatEditor)
-    .directive('configDialogflowEditor', configDialogflowEditor)
-    .directive('configDialogflowEsEditor', configDialogflowEsEditor)
-    .directive('configMessengerEditor', configMessengerEditor)
     .directive('configViberEditor', configViberEditor)
     .directive('configServiceMetaEditor', configServiceMetaEditor)
     .name;
