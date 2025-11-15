838d491b79dcfb4de13a07958420eda1eb364cd5
Tihomir Dmitrovic <tole.car@gmail.com>
2025-11-15 21:53:41 +0100

    Cleanup views a bit

--- resources/views/partials/navigation.php
+++ resources/views/partials/navigation.php
@@ -1,5 +1,5 @@
 <?php
-if ( ! defined('ABSPATH')) {
+if (! defined('ABSPATH')) {
     exit;
 }
 ?>
@@ -9,8 +9,8 @@ if ( ! defined('ABSPATH')) {
     <nav class="ops-left pl-4">
         <ul>
             <li class="d-flex align-items-center">
-                <a class="ops-logo" style="margin-top: 15px; margin-left: 15px;" href="<?php echo admin_url( 'admin.php?page=convo-plugin' ); ?>">
-                    <img width="35" height="22" src="<?php echo CONVOWP_ASSETS_URL?>/images/866fea890596f67cd1770b2686351816.png?866fea890596f67cd1770b2686351816)" alt=""Convoworks Logo>
+                <a class="ops-logo" style="margin-top: 15px; margin-left: 15px;" href="<?php echo admin_url('admin.php?page=convo-plugin'); ?>">
+                    <img width="35" height="22" src="<?php echo CONVOWP_ASSETS_URL ?>/images/866fea890596f67cd1770b2686351816.png?866fea890596f67cd1770b2686351816)" alt="Convoworks Logo" />
                     <div style="margin-left: 7px;font-family: DM Sans, sans-serif;width: 11px; color:#fff;display: inline;font-size: 15px;font-weight: bold;">Convoworks</div>
                 </a>
             </li>
