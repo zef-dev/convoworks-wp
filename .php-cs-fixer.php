beddbc1617440940471626cf699aae44288f3650
Tihomir Dmitrovic <tole.car@gmail.com>
2025-11-18 21:45:15 +0100

    Add php cs fixer

---
+++ .php-cs-fixer.php
@@ -0,0 +1,21 @@
+<?php
+
+$finder = PhpCsFixer\Finder::create()
+    ->in(__DIR__ . '/src')
+    ->name('*.php')
+    ->ignoreDotFiles(true)
+    ->ignoreVCS(true);
+
+return (new PhpCsFixer\Config())
+    ->setRiskyAllowed(false)
+    ->setRules([
+        '@PSR12' => true,
+        // Some sensible extras, tweak as you like
+        'array_syntax' => ['syntax' => 'short'],
+        'binary_operator_spaces' => [
+            'operators' => [
+                '=>' => 'align_single_space_minimal',
+            ],
+        ],
+    ])
+    ->setFinder($finder);
