2c1909e1d28c7a34b54bea1ecc13f80e0f2e3d4b
Tihomir Dmitrovic <tole.car@gmail.com>
2025-11-23 23:38:08 +0100

    Add helper method

--- src/Convo/Core/Expression/ExpressionFunction.php
+++ src/Convo/Core/Expression/ExpressionFunction.php
@@ -6,4 +6,21 @@ namespace Convo\Core\Expression;
 
 class ExpressionFunction extends \Symfony\Component\ExpressionLanguage\ExpressionFunction
 {
+    /**
+     * Creates an ExpressionFunction with a no-op compiler (for functions that don't need compilation)
+     * 
+     * @param string $name Function name
+     * @param callable $evaluator The evaluator function (receives $args as first param, then function params)
+     * @return ExpressionFunction
+     */
+    public static function fromEvaluator(string $name, callable $evaluator): self
+    {
+        return new self(
+            $name,
+            function () {
+                return ''; // No-op compiler
+            },
+            $evaluator
+        );
+    }
 }
