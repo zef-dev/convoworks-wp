3d8e688c039fc86ba28cbb8fc0727dca26624748
Tihomir Dmitrovic <tole.car@gmail.com>
2025-11-24 08:54:55 +0100

    Cleasnup unused

--- src/Convo/Wp/Helpers.php
+++ src/Convo/Wp/Helpers.php
@@ -52,31 +52,6 @@ function amazon_disconnect_url()
 }
 
 /**
- * Get the current domain
- *
- * @return string
- */
-function get_current_domain()
-{
-    $url = defined('WP_SITEURL') ? constant('WP_SITEURL') : 'http://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
-
-    return get_domain_from_url($url);
-}
-
-if (! function_exists('get_domain_from_url')) {
-    /**
-     * Parse URL and return domain
-     *
-     * @param  string $url
-     * @return string
-     */
-    function get_domain_from_url($url)
-    {
-        return parse_url($url, PHP_URL_HOST);
-    }
-}
-
-/**
  * Simply load an admin OPF view
  *
  * @param string $view
@@ -114,24 +89,5 @@ function is_convo_admin()
     return strpos($screen->id, 'convo-plugin') !== false or strpos($screen->id, 'convo-settings') !== false;
 }
 
-if (! function_exists('convo_can_edit_pages')) {
-    /**
-     * Check if current user can edit page
-     *
-     * @return bool
-     */
-    function convo_can_edit_pages()
-    {
-        return (bool) current_user_can('manage_convoworks');
-    }
-}
 
-function startsWith($haystack, $needle)
-{
-    return substr_compare($haystack, $needle, 0, strlen($needle)) === 0;
-}
-function endsWith($haystack, $needle)
-{
-    return substr_compare($haystack, $needle, -strlen($needle)) === 0;
-}
 
