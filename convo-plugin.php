379178a826dc25bd7ae1a1c3b3902086b605f6b1
Tihomir Dmitrovic <tole.car@gmail.com>
2025-11-24 17:44:20 +0100

    Update dev settings

--- convo-plugin.php
+++ convo-plugin.php
@@ -9,7 +9,7 @@
  * Plugin URI: https://convoworks.com
  * Update URI: https://convoworks.com/wp-content/uploads/convoworks/deploy/info.json
  * Author: ZEF Development
- * Version: 0.24.00-RC58
+ * Version: 0.24.00-RC60
  * Author URI: https://zef.dev
  * Text Domain: convoworks-wp
  * License: GPLv2 or later
@@ -36,7 +36,7 @@ if (!defined('CONVOWORKS_SECRET_KEY')) {
 use Convo\Wp\Providers\ConvoWPPlugin;
 use Convo\Wp\Providers\PluginActivator;
 
-define('CONVOWP_VERSION', '0.24.00-RC58');
+define('CONVOWP_VERSION', '0.24.00-RC60');
 define('CONVOWP_PLUGIN_SLUG', plugin_basename(__FILE__));
 define('CONVOWP_FILE', __FILE__);
 define('CONVOWP_PATH', __DIR__);
